7074346500fc6cd3e3e8505808f9d8dfd7d40abf
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-01-20 16:26:09 +0900

    [add]ページのレイアウトを作成

---
+++ src/app/components/app-header/Menu.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Menu() {
+  return (
+    <div className="flex items-centor¥">
+      <Link
+        href={"/"}
+        className="
+          btn md:w-32
+          title-small rounded-l-full
+        "
+      >
+        <h3>探す</h3>
+      </Link>
+      <Link
+        href={"/dash-board"}
+        className="
+          btn md:w-32
+          title-small rounded-none
+        "
+      >
+        <h3>確認</h3>
+      </Link>
+      <Link
+        href={"/exhibition"}
+        className="
+          btn md:w-32
+          title-small rounded-r-full
+        "
+      >
+        <h3>出品</h3>
+      </Link>
+    </div>
+  );
+}
